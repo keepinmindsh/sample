66f30d67b97cd767c0e5a132b48f1d974bc1f815
keepinmindsh@gmai.com <jyjsh0101+>
2020-04-28 16:57:58 +0900

    spring reactive web flux sample study

---
+++ spring/reactivewebflux/src/main/java/lines/reactivewebflux/sample01/Hello.java
@@ -0,0 +1,13 @@
+package lines.reactivewebflux.sample01;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Hello {
+    private String id;
+    private String name;
+}
---
+++ spring/reactivewebflux/src/main/java/lines/reactivewebflux/sample01/HelloWorldHandler.java
@@ -0,0 +1,22 @@
+package lines.reactivewebflux.sample01;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+@Slf4j
+public class HelloWorldHandler {
+    public Mono<ServerResponse> handleRequest(ServerRequest serverRequest) {
+
+        log.info("Call!");
+
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            log.error("에러 발생" + e.getLocalizedMessage());
+        }
+
+        return ServerResponse.ok().body(Mono.just("Hello World!"), String.class);
+    }
+}
