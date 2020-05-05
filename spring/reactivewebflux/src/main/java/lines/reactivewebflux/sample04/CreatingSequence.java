e9088ae54e2886781f09c4bfac5f9b027b2d3c6c
승화정 <keepinmindsh@gmail.com>
2020-05-05 13:48:21 +0900

    Reactive Web Flux Sample

---
+++ spring/reactivewebflux/src/main/java/lines/reactivewebflux/sample04/CreatingSequence.java
@@ -0,0 +1,48 @@
+package lines.reactivewebflux.sample04;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+@Slf4j
+public class CreatingSequence {
+    public static void main(String[] args) {
+        Flux<String> stringFlux = Flux.generate(
+                () -> 0,
+                (state, sink) -> {
+                    sink.next("3 x " + state + " = " + 3 * state);
+                    if( state == 10) sink.complete();
+                    return state + 1;
+                }
+        );
+
+        stringFlux.subscribe(item -> log.info("Generated Value : {}" , item) );
+
+
+        Flux<String> stringFlux1 = Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next(" 3 x " + i + " = " + 3 * i);
+                    if( i == 10 ) sink.complete();
+                    return state;
+                });
+
+        stringFlux1.subscribe(item -> log.info("Generated Value : {} " , item ));
+
+
+        Flux<String> stringFlux2 = Flux.generate(
+                AtomicLong::new,
+                (state, sink) -> {
+                    long i = state.getAndIncrement();
+                    sink.next(" 3 x " + i + " = " + 3 * i);
+                    if( i == 10 ) sink.complete();
+                    return state;
+                }, (state) -> log.info("state: {} ", state));
+
+        stringFlux2.subscribe(item -> log.info("Generated Value : {} " , item ));
+
+
+    }
+}
