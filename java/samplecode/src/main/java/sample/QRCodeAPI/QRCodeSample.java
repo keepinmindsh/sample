9daef72fdb36d940a72368a0e63d44afa7924d8d
keepinmindsh@gmai.com <jyjsh0101+>
2020-05-12 13:53:36 +0900

    QR API Sample

---
+++ java/samplecode/src/main/java/sample/QRCodeAPI/QRCodeSample.java
@@ -0,0 +1,35 @@
+package sample.QRCodeAPI;
+
+import com.google.zxing.BarcodeFormat;
+import com.google.zxing.WriterException;
+import com.google.zxing.client.j2se.MatrixToImageWriter;
+import com.google.zxing.common.BitMatrix;
+import com.google.zxing.qrcode.QRCodeWriter;
+
+import java.io.IOException;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+
+public class QRCodeSample {
+
+    private static final String QR_CODE_IMAGE_PATH = "./MyQRCode12.png";
+
+    private static void generateQRCodeImage(String text, int width, int height, String filePath)
+            throws WriterException, IOException {
+        QRCodeWriter qrCodeWriter = new QRCodeWriter();
+        BitMatrix bitMatrix = qrCodeWriter.encode("WINSPMS51119002109", BarcodeFormat.QR_CODE, width, height);
+
+        Path path = FileSystems.getDefault().getPath(filePath);
+        MatrixToImageWriter.writeToPath(bitMatrix, "PNG", path);
+    }
+
+    public static void main(String[] args) {
+        try {
+            generateQRCodeImage("This is my first QR Code", 350, 350, QR_CODE_IMAGE_PATH);
+        } catch (WriterException e) {
+            System.out.println("Could not generate QR Code, WriterException :: " + e.getMessage());
+        } catch (IOException e) {
+            System.out.println("Could not generate QR Code, IOException :: " + e.getMessage());
+        }
+    }
+}
