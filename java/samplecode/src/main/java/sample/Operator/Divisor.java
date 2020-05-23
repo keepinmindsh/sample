4d657eb56ed4057b48fffcaffb5f326aad19ce84
승화정 <keepinmindsh@gmail.com>
2020-05-23 17:57:16 +0900

    divisor 연산자 추가 테스트

---
+++ java/samplecode/src/main/java/sample/Operator/Divisor.java
@@ -0,0 +1,11 @@
+package sample.Operator;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Divisor {
+    public static void main(String[] args) {
+
+        log.info("Result : {}",  1 % 5);
+    }
+}
