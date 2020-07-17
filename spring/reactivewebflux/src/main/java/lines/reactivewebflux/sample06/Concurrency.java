51dcf87069f40b1ea1650984e68cd6bfeb08dc5d
승화정 <keepinmindsh@gmail.com>
2020-07-18 07:16:25 +0900

    Reactor - Study

---
+++ spring/reactivewebflux/src/main/java/lines/reactivewebflux/sample06/Concurrency.java
@@ -0,0 +1,18 @@
+package lines.reactivewebflux.sample06;
+
+import lombok.extern.slf4j.Slf4j;
+import reactor.core.publisher.Flux;
+import reactor.core.scheduler.Schedulers;
+
+@Slf4j
+public class Concurrency {
+    public static void main(String[] args) {
+        Flux.just(1, 2, 3, 4)
+                .log()
+                .map(i -> i * 2)
+                .subscribeOn(Schedulers.parallel())
+                .subscribe( item -> {
+                    log.info("Spring Data : {}" , item);
+                });
+    }
+}
---
+++ spring/reactivewebflux/src/main/java/lines/reactivewebflux/sample06/CreatingConnectableFlux.java
@@ -0,0 +1,20 @@
+package lines.reactivewebflux.sample06;
+
+import reactor.core.publisher.ConnectableFlux;
+import reactor.core.publisher.Flux;
+
+public class CreatingConnectableFlux {
+    public static void main(String[] args) {
+        ConnectableFlux<Object> publish = Flux.create(objectFluxSink -> {
+            while (true){
+                objectFluxSink.next(System.currentTimeMillis());
+            }
+        }).publish();
+
+        publish.subscribe(System.out::println);
+        publish.subscribe(System.out::println);
+
+        // connect()가 호출되기 전까지 위의 코드는 실행되지 않는다.
+        //publish.connect();
+    }
+}
