4bb8bb91d07ccab88b92fc8ce9acb52901547ca7
승화정 <keepinmindsh@gmail.com>
2020-07-18 08:00:48 +0900

    webflux-websocket-server & client 추가

---
+++ spring-netty/websocket-server/src/main/java/lines/websocket/event/Event.java
@@ -0,0 +1,11 @@
+package lines.websocket.event;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+@Data
+@AllArgsConstructor
+public class Event {
+    private String eventId;
+    private String eventDt;
+}
