f9637e0650ce7427557af9788a798ae81148d4fe
승화정 <keepinmindsh@gmail.com>
2020-07-30 22:20:05 +0900

    Pattern Study - Unify Interfaces with Adapter

---
+++ java/patternsample/src/main/java/pattern/sample/patternuse14/v1/weapon/Gun.java
@@ -0,0 +1,10 @@
+package pattern.sample.patternuse14.v1.weapon;
+
+import pattern.sample.patternuse14.v1.weapon.inf.Weapon;
+
+public class Gun implements Weapon {
+    @Override
+    public void checkStatus() {
+
+    }
+}
