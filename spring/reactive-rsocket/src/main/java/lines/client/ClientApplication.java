3e5761e9c92decae539132f1b136d8e5b845000b
승화정 <keepinmindsh@gmail.com>
2020-09-05 00:05:57 +0900

    Reactive - RSocker Test & Refactoring

---
+++ spring/reactive-rsocket/src/main/java/lines/client/ClientApplication.java
@@ -0,0 +1,108 @@
+package lines.client;
+import io.rsocket.SocketAcceptor;
+import io.rsocket.metadata.WellKnownMimeType;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.SneakyThrows;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.event.ApplicationReadyEvent;
+import org.springframework.boot.rsocket.messaging.RSocketStrategiesCustomizer;
+import org.springframework.context.ApplicationListener;
+import org.springframework.context.annotation.Bean;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.security.rsocket.metadata.SimpleAuthenticationEncoder;
+import org.springframework.security.rsocket.metadata.UsernamePasswordMetadata;
+import org.springframework.stereotype.Controller;
+import org.springframework.util.MimeType;
+import org.springframework.util.MimeTypeUtils;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+public class ClientApplication {
+
+    private final UsernamePasswordMetadata credentials = new UsernamePasswordMetadata("jlong", "pw");
+    private final MimeType mimeType = MimeTypeUtils.parseMimeType(WellKnownMimeType.MESSAGE_RSOCKET_AUTHENTICATION.getString());
+
+    @Bean
+    RSocketStrategiesCustomizer strategiesCustomizer() {
+        return strategies -> strategies.encoder(new SimpleAuthenticationEncoder());
+    }
+
+    @Bean
+    SocketAcceptor socketAcceptor(
+            RSocketStrategies strategies,
+            HealthController controller) {
+        return RSocketMessageHandler.responder(strategies, controller);
+    }
+
+    @Bean
+    RSocketRequester rSocketRequester(
+            SocketAcceptor acceptor,
+            RSocketRequester.Builder builder) {
+        return builder
+                .setupMetadata(this.credentials, this.mimeType)
+                .rsocketConnector(connector -> connector.acceptor(acceptor))
+                .connectTcp("localhost", 8888)
+                .block();
+    }
+
+    @Bean
+    ApplicationListener<ApplicationReadyEvent> client(RSocketRequester client) {
+        return args ->
+                client
+                        .route("greetings")
+                        .metadata(this.credentials, this.mimeType)
+                        .data(Mono.empty())
+//                        .data(new GreetingRequest("Alibaba"))
+                        .retrieveFlux(GreetingResponse.class)
+                        .subscribe(System.out::println);
+    }
+
+    @SneakyThrows
+    public static void main(String[] args) {
+        SpringApplication.run(ClientApplication.class, args);
+        System.in.read();
+    }
+
+}
+
+@Controller
+class HealthController {
+
+    @MessageMapping("health")
+    Flux<ClientHealthState> health() {
+        var stream = Stream.generate(() -> new ClientHealthState(Math.random() > .2));
+        return Flux.fromStream(stream).delayElements(Duration.ofSeconds(1));
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class ClientHealthState {
+    private boolean healthy;
+}
+
+// DTO
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class GreetingResponse {
+    private String message;
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class GreetingRequest {
+    private String name;
+}
---
+++ spring/reactive-rsocket/src/main/java/lines/service/ServiceApplication.java
@@ -0,0 +1,124 @@
+package lines.service;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.messaging.rsocket.RSocketRequester;
+import org.springframework.messaging.rsocket.RSocketStrategies;
+import org.springframework.messaging.rsocket.annotation.support.RSocketMessageHandler;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.rsocket.RSocketSecurity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.messaging.handler.invocation.reactive.AuthenticationPrincipalArgumentResolver;
+import org.springframework.security.rsocket.core.PayloadSocketAcceptorInterceptor;
+import org.springframework.stereotype.Controller;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
+import java.time.Duration;
+import java.time.Instant;
+import java.util.stream.Stream;
+
+@SpringBootApplication
+@PropertySource(value= {
+        "classpath:static/application.properties"
+})
+public class ServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServiceApplication.class, args);
+    }
+
+}
+
+
+@Configuration
+class SecurityConfiguration {
+
+
+    @Bean
+    PayloadSocketAcceptorInterceptor interceptor(RSocketSecurity security) {
+        return security
+                .simpleAuthentication(Customizer.withDefaults())
+                .authorizePayload(ap -> ap.anyExchange().authenticated())
+                .build();
+    }
+
+    @Bean
+    MapReactiveUserDetailsService authentication() {
+        return new MapReactiveUserDetailsService(User.withDefaultPasswordEncoder().username("jlong").password("pw").roles("USER").build());
+    }
+
+    @Bean
+    RSocketMessageHandler messageHandler(RSocketStrategies strategies) {
+        var rmh = new RSocketMessageHandler();
+        rmh.getArgumentResolverConfigurer().addCustomResolver(new AuthenticationPrincipalArgumentResolver());
+        rmh.setRSocketStrategies(strategies);
+        return rmh;
+    }
+}
+
+// DTO
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class GreetingResponse {
+    private String message;
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class GreetingRequest {
+    private String name;
+}
+
+@Controller
+@Log4j2
+class GreetingController {
+
+    @MessageMapping("greetings")
+    Flux<GreetingResponse> greet(
+            RSocketRequester clientRSocketConnection,
+            @AuthenticationPrincipal Mono<UserDetails> user) {
+        return user
+                .map(UserDetails::getUsername)
+                .map(GreetingRequest::new)
+                .flatMapMany(gr -> this.greet(clientRSocketConnection, gr));
+    }
+
+    private Flux<GreetingResponse> greet(
+            RSocketRequester clientRSocketConnection, GreetingRequest requests) {
+
+        var clientHealth = clientRSocketConnection
+                .route("health")
+                .retrieveFlux(ClientHealthState.class)
+                .filter(chs -> !chs.isHealthy())
+                .doOnNext(chs -> log.info("not healthy! "));
+
+        var greetings = Flux
+                .fromStream(Stream
+                        .generate(() -> new GreetingResponse("ni hao " + requests.getName() + " @ " + Instant.now() + "!")))
+                .take(100)
+                .delayElements(Duration.ofSeconds(1));
+
+        return greetings.takeUntilOther(clientHealth);
+    }
+}
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+class ClientHealthState {
+    private boolean healthy;
+}
