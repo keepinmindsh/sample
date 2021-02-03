49f7bdc47d874ebde37ac3b3888731eb884099e2
승화정 <keepinmindsh@gmail.com>
2021-02-04 06:48:50 +0900

    gateway sample

--- templates/lines-spring-gateway/api-service1/src/main/java/com/lines/api/controller/APIController.java
+++ templates/lines-spring-gateway/api-service1/src/main/java/com/lines/api/controller/APIController.java
@@ -3,6 +3,7 @@ package com.lines.api.controller;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.ServerHttpRequest;
 import org.springframework.http.server.ServerHttpResponse;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import reactor.core.publisher.Mono;
@@ -11,6 +12,7 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/api1")
 public class APIController {
 
+    @GetMapping("/call")
     public Mono<String> getAPI1Service(ServerHttpRequest request, ServerHttpResponse response){
         System.out.println("API1 Service");
         HttpHeaders headers = request.getHeaders();
