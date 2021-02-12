bd0f80d24a612b1d7bd14292497b1eb1239021a7
승화정 <keepinmindsh@gmail.com>
2021-02-13 04:07:35 +0900

    h2 DB Setting for Lines Credit Sample

--- templates/lines-card-handler/credit_handler/src/main/java/com/lines/credit/CreditWebApplication.java
+++ templates/lines-card-handler/credit_handler/src/main/java/com/lines/credit/CreditWebApplication.java
@@ -1,11 +1,35 @@
 package com.lines.credit;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.jdbc.core.JdbcTemplate;
+
+import javax.annotation.PostConstruct;
+import java.util.Arrays;
 
 @SpringBootApplication
 public class CreditWebApplication {
     public static void main(String[] args) {
         SpringApplication.run(CreditWebApplication.class);
+    }
+
+    @Autowired
+    private JdbcTemplate jdbcTemplate;
+
+    @PostConstruct
+    private void initDb() {
+        String sqlStatements[] = {
+                "drop table employees if exists",
+                "create table employees(id serial,first_name varchar(255),last_name varchar(255))",
+                "insert into employees(first_name, last_name) values('Eugen','Paraschiv')",
+                "insert into employees(first_name, last_name) values('Scott','Tiger')"
+        };
+
+        Arrays.asList(sqlStatements).forEach(sql -> {
+            jdbcTemplate.execute(sql);
+        });
+
+        // Query test data and print results
     }
 }
