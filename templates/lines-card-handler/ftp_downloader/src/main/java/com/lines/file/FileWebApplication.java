8fbb3a7d5c7b8ca5919a3e34d97b9ea5fbcc8b61
승화정 <keepinmindsh@gmail.com>
2021-02-14 20:38:57 +0900

    File Downloader - Process

---
+++ templates/lines-card-handler/ftp_downloader/src/main/java/com/lines/file/FileWebApplication.java
@@ -0,0 +1,12 @@
+package com.lines.file;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FileWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FileWebApplication.class);
+    }
+}
