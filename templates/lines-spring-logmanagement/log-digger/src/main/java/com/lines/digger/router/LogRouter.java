02bcd03d77cbdcf3b96c19ff77a434efb1c8283e
승화정 <keepinmindsh@gmail.com>
2021-03-11 05:07:01 +0900

    Creating Router Function with webflux

--- templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/router/LogRouter.java
+++ templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/router/LogRouter.java
@@ -1,40 +1,50 @@
-package com.lines.digger.messages;
+package com.lines.digger.router;
 
-import org.reactivestreams.Publisher;
-import org.reactivestreams.Subscription;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import reactor.core.publisher.Flux;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.reactive.function.server.RequestPredicates;
+import org.springframework.web.reactive.function.server.RouterFunction;
+import org.springframework.web.reactive.function.server.ServerResponse;
 
-@RestController
-@RequestMapping("/v1")
-public class Log {
+import static org.springframework.http.MediaType.APPLICATION_JSON;
+import static org.springframework.web.reactive.function.server.RequestPredicates.contentType;
 
-    @GetMapping("/logs")
-    public Object getLogs(){
-
-        Publisher<String> PBS = subscriber -> {
-            Subscription subscription = new Subscription() {
-                @Override
-                public void request(long n) {
-
-                    subscriber.onNext("abcd");
-
-                    subscriber.onComplete();
-                }
-                @Override
-                public void cancel() {
-
-                }
-            };
-
-            subscriber.onSubscribe(subscription);
-        };
+@Configuration
+public class LogRouter {
 
 
+    //TODO - https://d2.naver.com/helloworld/6080222 참고해서 Router 함수 적용하기
+    @Bean
+    public RouterFunction<ServerResponse> routes(){
 
-        return Flux.from(PBS)
-                .subscribe();
+        return RouterFunction.route(RequestPredicates.POST("/demo").and(contentType(APPLICATION_JSON)), null);
     }
+
+
+//    @GetMapping("/logs")
+//    public Object getLogs(){
+//
+//        Publisher<String> PBS = subscriber -> {
+//            Subscription subscription = new Subscription() {
+//                @Override
+//                public void request(long n) {
+//
+//                    subscriber.onNext("abcd");
+//
+//                    subscriber.onComplete();
+//                }
+//                @Override
+//                public void cancel() {
+//
+//                }
+//            };
+//
+//            subscriber.onSubscribe(subscription);
+//        };
+//
+//
+//
+//        return Flux.from(PBS)
+//                .subscribe();
+//    }
 }
