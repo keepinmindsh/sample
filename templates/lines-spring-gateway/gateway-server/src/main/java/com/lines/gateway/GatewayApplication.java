4a42ef3d2ea76cc3573286421e35fd0383b3a64c
승화정 <keepinmindsh@gmail.com>
2021-03-14 21:41:14 +0900

    Spring Cloud Loadbalancers

--- templates/lines-spring-gateway/gateway-server/src/main/java/com/lines/gateway/GatewayApplication.java
+++ templates/lines-spring-gateway/gateway-server/src/main/java/com/lines/gateway/GatewayApplication.java
@@ -9,7 +9,9 @@ public class GatewayApplication {
     // TODO http://dveamer.github.io/backend/SpringCloudGateway.html 참고 해서 부하분산 적용하기
 
     // TODO https://m.blog.naver.com/gngh0101/222106724959 Spring Cloud Loadbalancer
-    
+
+    // TODO https://github.com/spring-guides/gs-spring-cloud-loadbalancer 기본 자료 활용 - 샘플보고 적용할 것
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
