5f8205a240537c8e3876eddbcf4282c4304bdc8a
승화정 <keepinmindsh@gmail.com>
2021-03-14 21:35:07 +0900

    Spring Cloud - To do added

--- templates/lines-spring-gateway/gateway-server/src/main/java/com/lines/gateway/GatewayApplication.java
+++ templates/lines-spring-gateway/gateway-server/src/main/java/com/lines/gateway/GatewayApplication.java
@@ -5,6 +5,11 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class GatewayApplication {
+
+    // TODO http://dveamer.github.io/backend/SpringCloudGateway.html 참고 해서 부하분산 적용하기
+
+    // TODO https://m.blog.naver.com/gngh0101/222106724959 Spring Cloud Loadbalancer
+    
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
