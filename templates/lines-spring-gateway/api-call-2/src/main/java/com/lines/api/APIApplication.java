7870a4368adaca90e51face029cec79a2f85957d
승화정 <keepinmindsh@gmail.com>
2021-03-15 22:38:21 +0900

    LoadBalancers from API - Service 2  - api-call 1  - api-call 2

---
+++ templates/lines-spring-gateway/api-call-2/src/main/java/com/lines/api/APIApplication.java
@@ -0,0 +1,29 @@
+package com.lines.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+
+@RestController
+@SpringBootApplication
+public class APIApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(APIApplication.class, args);
+    }
+
+    @GetMapping("/greeting")
+    public String greet() {
+
+        List<String> greetings = Arrays.asList("Hi there1", "Greetings1", "Salutations1");
+        Random rand = new Random();
+
+        int randomNum = rand.nextInt(greetings.size());
+        return greetings.get(randomNum);
+    }
+
+}
