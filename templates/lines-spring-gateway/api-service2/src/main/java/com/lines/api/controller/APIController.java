bcf42cc4d2b5dcd6cb031bd124924188d2837e22
승화정 <keepinmindsh@gmail.com>
2021-03-15 22:37:28 +0900

    api-service 2   - route with load balancers

--- templates/lines-spring-gateway/api-service2/src/main/java/com/lines/api/controller/APIController.java
+++ templates/lines-spring-gateway/api-service2/src/main/java/com/lines/api/controller/APIController.java
@@ -1,16 +1,30 @@
 package com.lines.api.controller;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.cloud.client.loadbalancer.reactive.ReactorLoadBalancerExchangeFilterFunction;
 import org.springframework.http.HttpHeaders;
 import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.reactive.function.client.WebClient;
 import reactor.core.publisher.Mono;
 
 @RestController
 @RequestMapping("/api2")
+@RequiredArgsConstructor
 public class APIController {
+
+    private final WebClient.Builder loadBalancedWebClientBuilder;
+    private final ReactorLoadBalancerExchangeFilterFunction lbFunction;
+
+
+    public APIController(WebClient.Builder webClientBuilder,
+                           ReactorLoadBalancerExchangeFilterFunction lbFunction) {
+        this.loadBalancedWebClientBuilder = webClientBuilder;
+        this.lbFunction = lbFunction;
+    }
 
     @GetMapping("/call")
     public Mono<String> getAPI1Service(ServerHttpRequest request, ServerHttpResponse response){
@@ -23,6 +37,9 @@ public class APIController {
 
         Mono<String> data = Mono.just("Hello from Reactive API2");
 
-        return data;
+        return loadBalancedWebClientBuilder.build().get().uri("http://say-hello/greeting")
+                .retrieve().bodyToMono(String.class)
+                .map(greeting -> String.format("%s, %s!", greeting, "data!!"));
+
     }
 }
