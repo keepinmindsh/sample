63d05cc2385637b27d7a7f7a73096b112bdb9549
승화정 <keepinmindsh@gmail.com>
2021-03-16 22:29:44 +0900

    Reference Link added

--- templates/lines-spring-gateway/gateway-server/src/main/java/com/lines/gateway/GatewayApplication.java
+++ templates/lines-spring-gateway/gateway-server/src/main/java/com/lines/gateway/GatewayApplication.java
@@ -12,6 +12,12 @@ public class GatewayApplication {
 
     // TODO https://github.com/spring-guides/gs-spring-cloud-loadbalancer 기본 자료 활용 - 샘플보고 적용할 것
 
+    // TODO https://happycloud-lee.tistory.com/221 보고 참고해서 구성을 변경해볼 것
+
+    // TODO https://o7planning.org/11739/undertanding-load-balancing-in-spring-cloud-with-ribbon-and-example ribbon 구성
+
+    // TODO https://piotrminkowski.com/2020/05/13/a-deep-dive-into-spring-cloud-load-balancer/
+
     public static void main(String[] args) {
         SpringApplication.run(GatewayApplication.class, args);
     }
