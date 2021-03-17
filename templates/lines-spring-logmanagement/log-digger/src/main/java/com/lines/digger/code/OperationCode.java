18ba8b7601c00c6e191f788c1d2393ce44551526
승화정 <keepinmindsh@gmail.com>
2021-03-17 22:34:21 +0900

    Log Operator Definition with Enum

---
+++ templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/code/OperationCode.java
@@ -0,0 +1,9 @@
+package com.lines.digger.code;
+
+public enum OperationCode {
+    FILE_TREE,
+    FILE_OPEN,
+    FILE_ANALYZE,
+    RESERVATION_KEEP,
+    RESERVATION_GET
+}
--- templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/command/LogCommand.java
+++ templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/command/LogCommand.java
@@ -2,7 +2,7 @@ package com.lines.digger.command;
 
 import com.lines.lib.command.Command;
 
-// TODO Log Command 적용 - 파일을 읽어오는 역할을 하는 서비스 적용 
+// TODO Log Command 적용 - 파일을 읽어오는 역할을 하는 서비스 적용
 public class LogCommand implements Command {
     @Override
     public void execute() {
