c0336d4d91bc524f17045053a614d6ac17b61355
승화정 <keepinmindsh@gmail.com>
2021-03-25 22:42:23 +0900

    Sampled Added

--- templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/handler/LogHandler.java
+++ templates/lines-spring-logmanagement/log-digger/src/main/java/com/lines/digger/handler/LogHandler.java
@@ -19,13 +19,10 @@ public class LogHandler {
     private HashMap<Object, Object> result = new HashMap<>();
 
     public Mono<ServerResponse> post(ServerRequest serverRequest, OperationCode operationCode){
-
         Command command = new LogCommand(operationCode, serverRequest, result);
-
         command.execute();
 
         // TODO 응답이 정상적으로 반환되지 않음.
-
         return ServerResponse.ok().contentType(MediaType.APPLICATION_JSON).body(BodyInserters.fromProducer(command.result(), HashMap.class));
     }
 }
