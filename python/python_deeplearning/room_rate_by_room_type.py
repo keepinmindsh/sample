3b0ec1adbc94533e01fe0307df001a26f979af86
승화정 <keepinmindsh@gmail.com>
2021-06-06 22:09:20 +0900

    Case Study

--- python/python_deeplearning/room_rate_by_room_type.py
+++ python/python_deeplearning/room_rate_by_room_type.py
@@ -4,12 +4,11 @@ import numpy as np
 import matplotlib.pyplot as plt
 import seaborn as sns
 
-df = pd.read_csv("ROOM_RATE 3.csv")
+df = pd.read_csv("ROOM_RATE 4.csv")
 print(df.head())
 print(df.info())
 
 df_cleaned = df.dropna(how='all')
-
 
 X = df_cleaned["LODG_DATE"][:50]
 y = df_cleaned["ROOM_FEE"][:50]
--- python/python_deeplearning/room_rate_total_by_date.py
+++ python/python_deeplearning/room_rate_total_by_date.py
@@ -2,18 +2,33 @@ from sklearn.linear_model import LinearRegression
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
+import datetime
 
-df = pd.read_csv("ROOM_RATE 3.csv")
+df = pd.read_csv("ROOM_RATE 4.csv")
 print(df.head())
 
 
 df_cleaned = df.dropna(how='all')
-df_typecasting =  df_cleaned.astype({"LODG_DATE":'int64'})
+#df_typecasting =  df_cleaned.astype({"LODG_DATE":'int64'})
 
-print(df_typecasting.info())
+#df_cleaned = df_cleaned.set_index('LODG_DATE', append=False)
+#df_cleaned = df_cleaned.index.to_julian_date()
+#print(pd.to_datetime(df_cleaned.LODG_DATE, errors='coerce', format='%m%d%Y'))
+#$df_cleaned['LODG_DATE'] = pd.to_datetime(df_cleaned.LODG_DATE, format='%m%d%Y')
+#df_cleaned['LODG_DATE'] = df_cleaned['LODG_DATE'].map(datetime.datetime.toordinal)
 
-X = df_typecasting["LODG_DATE"][:50]
-y = df_typecasting["ROOM_FEE"][:50]
+#df_cleaned = df_cleaned.astype({'LODG_DATE': 'int32'})
+
+print(pd.to_datetime(df_cleaned['LODG_DATE'], format='%Y-%m-%d'))
+print(pd.to_datetime(df_cleaned['LODG_DATE'], format='%Y-%m-%d'))
+
+#print(df_cleaned.LODG_DATE.values.astype(np.int64))
+
+#print(df_cleaned.info())
+#print(df_cleaned.head())
+
+X = df_cleaned["LODG_DATE"][:50]
+y = df_cleaned["ROOM_FEE"][:50]
 #plt.plot(X, y, 'o')
 #plt.show()
 
@@ -22,7 +37,7 @@ line_fitter.fit(X.values.reshape(-1,1), y)
 
 print(line_fitter.predict([[20120429]]))
 
-plt.figure(figsize=(20, 5))
+#plt.figure(figsize=(20, 5))
 plt.plot(X, y, 'o')
 plt.plot(X,line_fitter.predict(X.values.reshape(-1,1)))
 plt.show()
