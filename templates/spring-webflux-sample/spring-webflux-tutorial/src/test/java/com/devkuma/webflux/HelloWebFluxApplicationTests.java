0360eb588a7e50cf9fea7bd94423e53bc0479b94
lines_makers <keepinmindsh@gmail.com>
2021-08-16 12:26:17 +0900

    webflux-sample

---
+++ templates/spring-webflux-sample/spring-webflux-tutorial/src/test/java/com/devkuma/webflux/HelloWebFluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.devkuma.webflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HelloWebFluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
