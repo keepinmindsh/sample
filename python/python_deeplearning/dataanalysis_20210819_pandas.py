02d8f9d8bc498a8cfd754fea11227094c53e3926
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2021-08-20 08:23:11 +0900

    python for pandas basic

---
+++ python/python_deeplearning/dataanalysis_20210819_pandas.py
@@ -0,0 +1,22 @@
+import pandas as pd
+
+df = pd.DataFrame(
+    {
+        "Name": [
+            "Braund, Mr. Owen Harris",
+            "Allen, Mr. William Henry",
+            "Bonnell, Miss. Elizabeth",
+        ],
+        "Age": [22, 35, 58],
+        "Sex": ["male", "male", "female"],
+    }
+)
+
+print(df)
+
+
+print(df["Age"])
+
+print(df["Age"].max())
+
+print(df.describe())
