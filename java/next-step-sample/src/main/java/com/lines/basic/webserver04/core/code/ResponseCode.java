5728fd99db598c2ace10a4edee08ca3f021510e3
lines_makers <keepinmindsh@gmail.com>
2021-09-04 15:28:30 +0900

    code clear for next step code

--- java/next-step-sample/src/main/java/com/lines/basic/webserver04/core/code/ResponseCode.java
+++ java/next-step-sample/src/main/java/com/lines/basic/webserver04/core/code/ResponseCode.java
@@ -3,7 +3,8 @@ package com.lines.basic.webserver04.core.code;
 public enum ResponseCode {
     HTTP_200("200"),
     HTTP_301("301"),
-    HTTP_302("302");
+    HTTP_302("302"),
+    HTTP_500("500");
 
     private final String value;
 
