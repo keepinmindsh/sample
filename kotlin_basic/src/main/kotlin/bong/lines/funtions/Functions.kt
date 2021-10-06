90ebdfaeede7f94f0973576dbf5dae61d7cfc11e
lines_makers <keepinmindsh@gmail.com>
2021-10-07 06:07:09 +0900

    kotlin basic for functions

--- kotlin_basic/src/main/kotlin/bong/lines/funtions/Functions.kt
+++ kotlin_basic/src/main/kotlin/bong/lines/funtions/Functions.kt
@@ -4,6 +4,22 @@ fun sum(a: Int, b: Int): Int {
     return a + b
 }
 
+fun sum1(a: Int, b: Int): Unit {
+    print("sum of $a and $b is ${a + b}")
+}
+
+fun sum2(a: Int, b: Int) = a + b
+
+fun sum3(a: Int, b: Int) {
+    println("sum of $a and $b is ${a + b}")
+}
+
 fun main(){
-    print(sum(1, 2))
+    println(sum(1, 2))
+
+    println(sum1(1,4))
+
+    println(sum2(5,4))
+
+    println(sum3(9,4))
 }
