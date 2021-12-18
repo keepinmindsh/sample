fb7b2c542c121a2815cb59adb635832cff4602ff
승화정 <keepinmindsh@gmail.com>
2021-12-18 22:14:13 +0900

    kotlin classes

---
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/enumclass/EnumClass.kt
@@ -0,0 +1,30 @@
+package bong.lines.basic.enumclass
+
+enum class State {
+    IDLE, RUNNING, FINISHED                           // 1
+}
+
+enum class Color(val rgb: Int) {                      // 1
+    RED(0xFF0000),                                    // 2
+    GREEN(0x00FF00),
+    BLUE(0x0000FF),
+    YELLOW(0xFFFF00);
+
+    fun containsRed() = (this.rgb and 0xFF0000 != 0)  // 3
+}
+
+fun main() {
+    val state = State.RUNNING // 2
+    val message = when (state) {                      // 3
+        State.IDLE -> "It's idle"
+        State.RUNNING -> "It's running"
+        State.FINISHED -> "It's finished"
+    }
+    println(message)
+
+    val red = Color.RED
+    println(red)                                      // 4
+    println(red.containsRed())                        // 5
+    println(Color.BLUE.containsRed())                 // 6
+    println(Color.YELLOW.containsRed())               // 7
+}
---
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/equalitycheck/EqualityCheck.kt
@@ -0,0 +1,9 @@
+package bong.lines.basic.equalitycheck
+
+fun main() {
+    val authors = setOf("Shakespeare", "Hemingway", "Twain")
+    val writers = setOf("Twain", "Shakespeare", "Hemingway")
+
+    println(authors == writers)   // 1
+    println(authors === writers)  // 2
+}
