e593123776f8c621466676df29efd48b5bf8a042
승화정 <keepinmindsh@gmail.com>
2021-12-18 22:00:23 +0900

    kotlin for inheritances

---
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/inheritance/Inheritances.kt
@@ -0,0 +1,40 @@
+package bong.lines.basic.inheritance
+
+open class Dog {                // 1
+    open fun sayHello() {       // 2
+        println("wow wow!")
+    }
+}
+
+class Yorkshire : Dog() {       // 3
+    override fun sayHello() {   // 4
+        println("wif wif!")
+    }
+}
+
+open class Tiger(val origin: String) {
+    fun sayHello() {
+        println("A tiger from $origin says: grrhhh!")
+    }
+}
+
+class SiberianTiger : Tiger("Siberia")                  // 1
+
+open class Lion(val name: String, val origin: String) {
+    fun sayHello() {
+        println("$name, the lion from $origin says: graoh!")
+    }
+}
+
+class Asiatic(name: String) : Lion(name = name, origin = "India") // 1
+
+fun main() {
+    val dog: Dog = Yorkshire()
+    dog.sayHello()
+
+    val tiger: Tiger = SiberianTiger()
+    tiger.sayHello()
+
+    val lion: Lion = Asiatic("Rufo")                              // 2
+    lion.sayHello()
+}
