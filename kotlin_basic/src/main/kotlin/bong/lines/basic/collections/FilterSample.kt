dcc8fdb95b2b20439f12f6530e840d914892b3ff
승화정 <keepinmindsh@gmail.com>
2021-12-25 23:08:17 +0900

    kotlin basic sample for collections

---
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/collections/FilterSample.kt
@@ -0,0 +1,15 @@
+package bong.lines.basic.collections
+
+val numbers = listOf(1, -2, 3, -4, 5, -6)      // 1
+
+val positives = numbers.filter { x -> x > 0 }  // 2
+
+val negatives = numbers.filter { it < 0 }      // 3
+
+fun main() {
+    println(numbers)
+
+    println(positives)
+
+    println(negatives)
+}
---
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/collections/Map2Sample.kt
@@ -0,0 +1,17 @@
+package bong.lines.basic.collections
+
+
+fun main() {
+
+    val numbers = listOf(1, -2, 3, -4, 5, -6)     // 1
+
+    val doubled = numbers.map { x -> x * 2 }      // 2
+
+    val tripled = numbers.map { it * 3 }          // 3
+
+    println(numbers)
+
+    println(doubled)
+
+    println(tripled)
+}
