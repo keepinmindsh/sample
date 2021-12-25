3449f892d5abafdc4ffd04eadf50f276c50436bd
승화정 <keepinmindsh@gmail.com>
2021-12-25 23:28:04 +0900

    kotlin basic sample for collections

--- kotlin_basic/src/main/kotlin/bong/lines/basic/collections/FirstLastSample.kt
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/collections/FirstLastSample.kt
@@ -1,6 +1,9 @@
 package bong.lines.basic.collections
 
 fun main() {
+    /**
+     * These functions return the first and the last element of the collection correspondingly. You can also use them with a predicate; in this case, they return the first or the last element that matches the given predicate.s
+     */
     val numbers = listOf(1, -2, 3, -4, 5, -6)            // 1
 
     val first = numbers.first()                          // 2
@@ -16,4 +19,38 @@ fun main() {
     println(firstEven)
 
     println(lastOdd)
+
+    println()
+
+    /**
+     * These functions work almost the same way with one difference: they return null if there are no matching elements.
+     */
+    val words = listOf("foo", "bar", "baz", "faz")         // 1
+    val empty = emptyList<String>()                        // 2
+
+    val firstOrNull = empty.firstOrNull()                        // 3
+    val lastOrNull = empty.lastOrNull()                          // 4
+
+    val firstF = words.firstOrNull { it.startsWith('f') }  // 5
+    val firstZ = words.firstOrNull { it.startsWith('z') }  // 6
+    val lastF = words.lastOrNull { it.endsWith('f') }      // 7
+    val lastZ = words.lastOrNull { it.endsWith('z') }      // 8
+
+    println(firstOrNull)
+    println(lastOrNull)
+    println(firstF)
+    println(firstZ)
+    println(lastF)
+    println(lastZ)
+
+    println()
+
+    /**
+     * count functions returns either the total number of elements in a collection or the number of elements matching the given predicate.
+     */
+    val totalCount = numbers.count()                     // 2
+    val evenCount = numbers.count { it % 2 == 0 }        // 3
+
+    println(totalCount)
+    println(evenCount)
 }
