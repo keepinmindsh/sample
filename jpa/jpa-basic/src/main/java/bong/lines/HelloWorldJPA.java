afc07574595f81d1cc966a1fe6de4c3b756d7300
Jeong Seung Hwa <keepinmindsh@gmail.com>
2022-01-04 08:27:53 +0900

    JPA Study

--- jpa/jpa-basic/src/main/java/bong/lines/HelloWorldJPA.java
+++ jpa/jpa-basic/src/main/java/bong/lines/HelloWorldJPA.java
@@ -1,7 +1,19 @@
 package bong.lines;
 
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+
 public class HelloWorldJPA {
     public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
 
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+
+
+        entityManager.close();
+
+        entityManagerFactory.close();
     }
 }
