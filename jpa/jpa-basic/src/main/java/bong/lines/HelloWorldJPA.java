dced431e241226a62dbaf2c9c66a0a8e04970d70
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-01-05 06:51:33 +0900

    jpa basic

--- jpa/jpa-basic/src/main/java/bong/lines/HelloWorldJPA.java
+++ jpa/jpa-basic/src/main/java/bong/lines/HelloWorldJPA.java
@@ -16,12 +16,23 @@ public class HelloWorldJPA {
         entityTransaction.begin();
 
         try{
-            Member member = new Member();
+            // TODO 저장 클래스 분리
+            //Member member = new Member();
+            //member.setId(2L);
+            //member.setName("Hello");
+            //entityManager.persist(member);
 
-            member.setId(2L);
-            member.setName("Hello");
+            // TODO 조회 클래스 분리
+            //Member findMember = entityManager.find(Member.class, 1L);
+            //System.out.println("Id :" + findMember.getId() + ", Name : " + findMember.getName());
 
-            entityManager.persist(member);
+            // TODO 삭제
+            //Member findMember = entityManager.find(Member.class, 1L);
+            //entityManager.remove(findMember);
+
+            // TODO 수정 - JPA를 통해서 객체를 반환하면 해당 객체는 JPA에서 관리하여 변경사항을 Transaction Commit시 반영한다.
+            Member findMember = entityManager.find(Member.class, 1L);
+            findMember.setName("Good!");
 
             entityTransaction.commit();
         }catch (Exception exception){
