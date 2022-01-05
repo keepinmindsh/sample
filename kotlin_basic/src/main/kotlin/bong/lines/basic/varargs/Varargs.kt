4b0307322ba1a75d4d4e832d5cda282763b99b26
Jeong Seung Hwa <keepinmindsh@gmail.com>
2022-01-06 08:34:22 +0900

    Varargs 기능 추가

--- kotlin_basic/src/main/kotlin/bong/lines/basic/varargs/Varargs.kt
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/varargs/Varargs.kt
@@ -1,5 +1,6 @@
 package bong.lines.basic.varargs
 
+
 fun printAll(vararg messages: String) {                            // 1
     for (m in messages) println(m)
 }
@@ -8,10 +9,15 @@ fun printAllWithPrefix(vararg messages: String, prefix: String) {  // 3
     for (m in messages) println(prefix + m)
 }
 
+fun printAllWithObject(vararg params: Int){
+    for(m in params) println("Object is ${m}")
+}
+
 fun log(vararg entries: String) {
     printAll(*entries)                                             // 5
 }
 
+// TODO - https://velog.io/@milkcoke/Kotlin-varargs 참고 할 것 
 fun main() {
     printAll("Hello", "Hallo", "Salut", "Hola", "你好")                 // 2
 
@@ -19,6 +25,8 @@ fun main() {
         "Hello", "Hallo", "Salut", "Hola", "你好",
         prefix = "Greeting: "                                          // 4
     )
+
+    printAllWithObject(1,123, 324)
 
     log("Hello", "Hallo", "Salut", "Hola", "你好")
 }
