f84a9d4ef6f0199b17621706f34193305e415310
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-01-09 15:39:47 +0900

    kotlin basic study

--- kotlin_basic/src/main/kotlin/bong/lines/basic/conditional_expression/ConditionalExpressions.kt
+++ kotlin_basic/src/main/kotlin/bong/lines/basic/conditional_expression/ConditionalExpressions.kt
@@ -4,6 +4,8 @@ fun main() {
     println(maxOf1(10, 20))
 
     println(maxOf2(10, 20))
+
+    println(maxOf3(30,40))
 }
 
 fun maxOf1(a: Int, b:Int) = if ( a > b ) a else b
@@ -14,4 +16,16 @@ fun maxOf2(a: Int, b: Int): Int {
     } else {
         return b
     }
+}
+
+fun maxOf3(a: Int, b: Int): Int {
+    val max = if (a > b) {
+        print("Choose a")
+        a
+    } else {
+        print("Choose b")
+        b
+    }
+
+    return max
 }
