a3ca926fd58f38c7d6d139e3f647fa7099693002
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-02-02 08:14:57 +0900

    jpa basic - OneToMany

---
+++ jpa/jpa-basic/src/main/java/bong/lines/jpashoping/step5/JPAMain.java
@@ -0,0 +1,42 @@
+package bong.lines.jpashoping.step5;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import java.util.List;
+
+public class JPAMain {
+    public static void main(String[] args) {
+        EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("hello");
+
+        EntityManager entityManager = entityManagerFactory.createEntityManager();
+
+        EntityTransaction entityTransaction = entityManager.getTransaction();
+
+        entityTransaction.begin();
+
+        try{
+
+            Member member = new Member();
+            member.setUsername("bong1");
+
+            entityManager.persist(member);
+
+            Team team = new Team();
+
+            team.setName("Bong Team");
+            team.getMembers().add(member);
+
+            entityManager.persist(team);
+
+            entityTransaction.commit();
+        }catch (Exception exception){
+            entityTransaction.rollback();
+        }finally {
+            entityManager.close();
+        }
+
+        entityManagerFactory.close();
+    }
+}
