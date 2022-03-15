56b4e63406d59d334d39b3b279f8b0707e420625
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-03-15 22:56:22 +0900

    jpa basic - valuetype collection

--- jpa/jpa-basic/src/main/java/bong/lines/jpashoping/valuetypecollection/JPAMain.java
+++ jpa/jpa-basic/src/main/java/bong/lines/jpashoping/valuetypecollection/JPAMain.java
@@ -45,6 +45,23 @@ public class JPAMain {
                 System.out.println("address.getCity() = " + address.getCity());
             }
 
+            // 값타입은 아래와 같이 변경은 불가함.
+            // findMember.getHomeAddress().setCity("newCity");
+
+            // 아래의 코드가 정상적인 방식
+            Address address = findMember.getHomeAddress();
+            findMember.setHomeAddress(new Address("new City", address.getStreet(), address.getZipcode()));
+
+            // 치킨 -> 한식
+            findMember.getFavoriteFood().remove("치킨");
+            findMember.getFavoriteFood().add("한식");
+
+            // 주소 변경 처리
+            // 해당 부분에서 아래의 쿼리의 동작이 관련 member_id를 모두 지우고 다시 아래 값을 입력함.
+            //
+            findMember.getAddressHistory().remove(new Address("city2", "Street 1", "zip code 1"));
+            findMember.getAddressHistory().add(new Address("newCity1", "Street 1", "zip code 1"));
+
             entityTransaction.commit();
 
         }catch (Exception exception){
