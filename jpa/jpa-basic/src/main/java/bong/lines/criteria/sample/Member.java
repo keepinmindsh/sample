10cfe79cafa56053346c58799abcf346adb0d820
DESKTOP-1JFMACK\shjeong-PC <keepinmindsh@gmail.com>
2022-03-19 11:39:14 +0900

    jpa basic - query dsl and criteria introduction

---
+++ jpa/jpa-basic/src/main/java/bong/lines/criteria/sample/Member.java
@@ -0,0 +1,34 @@
+package bong.lines.criteria.sample;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Member {
+
+    @Id
+    @GeneratedValue
+    @Column(name="MEMBER_ID")
+    private Long id;
+
+    @Column(name="USERNAME")
+    private String userName;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String username) {
+        this.userName = username;
+    }
+}
