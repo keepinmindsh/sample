9e28becefd03315988ced7e4a184f4af3a92a131
SEUNGHWA JEONG <keepinmindsh@gmail.com>
2022-06-08 23:16:53 +0900

    remove ktor project:

--- kotlin_basic/ktor_websocket/src/main/java/com/example/Application.kt
+++
@@ -1,8 +0,0 @@
-package com.example
-
-fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
-
-fun Application.module() {
-    configureRouting()
-    configureSerialization()
-}
